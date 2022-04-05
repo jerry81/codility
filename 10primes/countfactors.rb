380104895203e7dca445fc2664389e7d9e8be55c
Raskolnikov <jerry.tann@endress.com>
2022-04-05 12:53:52 +0800

    📝 revise the answer

--- 10primes/countfactors.rb
+++ 10primes/countfactors.rb
@@ -24,10 +24,61 @@ def solution(input)
             return count
         end
         if input % i == 0
-            count += 2
+            if i**2 == input
+                count += 1
+            else
+                count += 2
+            end
         end
     end
     count 
 end
 
 puts "expect 8 : #{solution(24)}"
+puts "expect 5 : #{solution(16)}"
+
+=begin 
+
+results:  wrong answers for 
+squares
+N=16, N=36✘WRONG ANSWER
+got 6 expected 5
+▶tiny
+N <= 10✘WRONG ANSWER
+got 2 expected 1
+▶simple1
+N=41(prime), N=42✔OK
+▶simple2
+N=69, N=64, N=120=5!✘WRONG ANSWER
+got 8 expected 7
+▶simple3
+N=720=6!, N=1111✔OK
+1.0.044 sOK
+2.0.048 sOK
+▶simple4
+N=5,040=7!, N=12,345✔OK
+▶simple5
+N=34,879, N=40,320=8!✔OK
+▶extreme_one
+N=1✘WRONG ANSWER
+got 2 expected 1
+expand allPerformance tests
+▶medium1
+N=362,880=9!, N=1,948,102✔OK
+▶medium2
+N=3,628,800=10!, N=5,621,892, N=4,999,696✘WRONG ANSWER
+got 46 expected 45
+▶big1
+N=27,043,111, N=39,916,800=11!, N = 39,992,976✘WRONG ANSWER
+got 136 expected 135
+▶big2
+N=97,093,212, N=2^28✘WRONG ANSWER
+got 30 expected 29
+▶big3
+N=479,001,600=12!, N=780291637(prime), N=449,991,369✘WRONG ANSWER
+got 16 expected 15
+▶extreme_maxint
+N=1,000,000,000, N=MAX_INT, N=2147,395,600✘WRONG ANSWER
+got 136 expected 135
+
+=end
