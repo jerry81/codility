470aceb364a33838ee7d56e48e4371fca44805a1
Raskolnikov <jerry.tann@endress.com>
2022-04-23 14:15:41 +0800

    🐛 use sum method

--- Ex6/sqlsum.sql
+++ Ex6/sqlsum.sql
@@ -1,5 +1,27 @@
+/* 
+Given a table elements with the following structure:
+
+  create table elements (
+      v integer not null
+  );
+write an SQL query that returns the sum of the numbers in column v.
+
+For example, given:
+
+  v
+  ---
+  2
+  10
+  20
+  10
+your query should return 42.
+
+use https://extendsclass.com/postgresql-online.html
+to play with pg online 
+*/
+
 create table elements (v integer);
-		insert into element (vb) values (1);
-		insert into element (id, arb) values (3);
-		select * from elements;
+		insert into elements (v) values (1);
+		insert into elements (v) values (3); 
+		SELECT SUM (v) as total from elements;
         
