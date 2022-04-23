7be40e3fb28c6e529146767fdfd39b8cda99b694
Raskolnikov <jerry.tann@endress.com>
2022-04-23 14:33:30 +0800

    ✨ start string symmetry problem

---
+++ Ex4/strsymmetry.rb
@@ -0,0 +1,41 @@
+=begin
+  Write a function:
+
+def solution(s)
+
+that, given a string S, returns the index (counting from 0) of a character such that the part of the string to the left of that character is a reversal of the part of the string to its right. The function should return −1 if no such index exists.
+
+Note: reversing an empty string (i.e. a string whose length is zero) gives an empty string.
+
+For example, given a string:
+
+"racecar"
+
+the function should return 3, because the substring to the left of the character "e" at index 3 is "rac", and the one to the right is "car".
+
+Given a string:
+
+"x"
+
+the function should return 0, because both substrings are empty.
+
+Write an efficient algorithm for the following assumptions:
+
+the length of string S is within the range [0..2,000,000].
+=end
+
+def solution(a)
+    if a.length % 2 == 0
+        return -1
+    end
+
+    
+end
+
+puts "expect 0 #{solution("x")}"
+
+puts "expect 3 #{solution("racecar")}"
+
+puts "expect -1 #{solution("aa")}"
+
+puts "expect -1 "{solution("abc") }"
