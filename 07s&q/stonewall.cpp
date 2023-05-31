81a47fc9c56abc83b18c5d3948ef9f5758105d2a
Raskolnikov <jerry.tann@endress.com>
2023-05-31 15:47:50 +0800

    🐛 o really, this is easy?

--- 07s&q/stonewall.cpp
+++ 07s&q/stonewall.cpp
@@ -1,21 +1,30 @@
 /*
 
-You are going to build a stone wall. The wall should be straight and N meters long, and its thickness should be constant; however, it should have different heights in different places. The height of the wall is specified by an array H of N positive integers. H[I] is the height of the wall from I to I+1 meters to the right of its left end. In particular, H[0] is the height of the wall's left end and H[N−1] is the height of the wall's right end.
+You are going to build a stone wall. The wall should be straight and N meters
+long, and its thickness should be constant; however, it should have different
+heights in different places. The height of the wall is specified by an array H
+of N positive integers. H[I] is the height of the wall from I to I+1 meters to
+the right of its left end. In particular, H[0] is the height of the wall's left
+end and H[N−1] is the height of the wall's right end.
 
-The wall should be built of cuboid stone blocks (that is, all sides of such blocks are rectangular). Your task is to compute the minimum number of blocks needed to build the wall.
+The wall should be built of cuboid stone blocks (that is, all sides of such
+blocks are rectangular). Your task is to compute the minimum number of blocks
+needed to build the wall.
 
 Write a function:
 
 int solution(vector<int> &H);
 
-that, given an array H of N positive integers specifying the height of the wall, returns the minimum number of blocks needed to build it.
+that, given an array H of N positive integers specifying the height of the wall,
+returns the minimum number of blocks needed to build it.
 
 For example, given array H containing N = 9 integers:
 
   H[0] = 8    H[1] = 8    H[2] = 5
   H[3] = 7    H[4] = 9    H[5] = 8
   H[6] = 7    H[7] = 4    H[8] = 8
-the function should return 7. The figure shows one possible arrangement of seven blocks.
+the function should return 7. The figure shows one possible arrangement of seven
+blocks.
 
 
 
@@ -26,15 +35,16 @@ each element of array H is an integer within the range [1..1,000,000,000].
 
 */
 
-#include <vector>
+#include <iostream>
 #include <stack>
+#include <vector>
 
 using namespace std;
 
 int solution(vector<int> &H) {
   int count = 0;
   stack<int> stk;
-  for (int i: H) {
+  for (int i : H) {
     if (stk.empty()) {
       stk.push(i);
       count++;
@@ -48,8 +58,8 @@ int solution(vector<int> &H) {
       }
       if (stk.empty()) {
         stk.push(i);
+        count++;
       }
-      count++;
       continue;
     }
     if (i == t) continue;
@@ -60,3 +70,45 @@ int solution(vector<int> &H) {
   }
   return count;
 }
+
+int main() {
+  vector<int> test1 = {1, 2, 3, 3, 2, 1};
+  cout << "expect 3 " << solution(test1) << endl;
+  vector<int> test2 = {1, 1000000000, 1};
+  cout << "expect 2 " << solution(test2) << endl;
+  vector<int> example = {8,8,5,7,9,8,7,4,8};
+  cout << "expect 7 " << solution(example) << endl;
+}
+
+/*
+
+failures
+expect 3
+1,
+2,
+3,
+3,
+2,
+1,
+
+expect 2
+1,
+1000000000,
+1,
+
+expect 5
+
+1,
+1,
+1,
+2,
+3,
+3,
+2,
+1,
+2,
+3,
+3,
+3,
+
+*/
