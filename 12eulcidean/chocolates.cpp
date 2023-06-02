745a87b964eddcee2c37b09d699a981ca585bd45
Raskolnikov <jerry.tann@endress.com>
2023-06-02 15:07:29 +0800

    🚧 could this be it?

--- 12eulcidean/chocolates.cpp
+++ 12eulcidean/chocolates.cpp
@@ -1,26 +1,34 @@
 /*
 
-Two positive integers N and M are given. Integer N represents the number of chocolates arranged in a circle, numbered from 0 to N − 1.
+Two positive integers N and M are given. Integer N represents the number of
+chocolates arranged in a circle, numbered from 0 to N − 1.
 
-You start to eat the chocolates. After eating a chocolate you leave only a wrapper.
+You start to eat the chocolates. After eating a chocolate you leave only a
+wrapper.
 
-You begin with eating chocolate number 0. Then you omit the next M − 1 chocolates or wrappers on the circle, and eat the following one.
+You begin with eating chocolate number 0. Then you omit the next M − 1
+chocolates or wrappers on the circle, and eat the following one.
 
-More precisely, if you ate chocolate number X, then you will next eat the chocolate with number (X + M) modulo N (remainder of division).
+More precisely, if you ate chocolate number X, then you will next eat the
+chocolate with number (X + M) modulo N (remainder of division).
 
 You stop eating when you encounter an empty wrapper.
 
-For example, given integers N = 10 and M = 4. You will eat the following chocolates: 0, 4, 8, 2, 6.
+For example, given integers N = 10 and M = 4. You will eat the following
+chocolates: 0, 4, 8, 2, 6.
 
-The goal is to count the number of chocolates that you will eat, following the above rules.
+The goal is to count the number of chocolates that you will eat, following the
+above rules.
 
 Write a function:
 
 int solution(int N, int M);
 
-that, given two positive integers N and M, returns the number of chocolates that you will eat.
+that, given two positive integers N and M, returns the number of chocolates that
+you will eat.
 
-For example, given integers N = 10 and M = 4. the function should return 5, as explained above.
+For example, given integers N = 10 and M = 4. the function should return 5, as
+explained above.
 
 Write an efficient algorithm for the following assumptions:
 
@@ -38,28 +46,33 @@ int solution(int N, int M) {
   int cur = 0;
   bool start = false;
   int iterations = 0;
-  int count = 0;
+  int count = 1;
+  int period = N / M;
+  int interval = period * M;
   while (iterations < 200) {
     iterations++;
     if ((cur == 0 || cur == N) && start) {
       return count;
     }
     start = true;
-    cur+=M;
 
-    count++;
-    if (cur > N) {
-      cur = cur%N;
+    count += period;
+    cur += interval;
+
+    if (cur < N) cur += M;
+
+    if (cur >= N) {
+      cur = cur % N;
     }
-
+    iterations++;
   }
 
   return count;
 }
 
 int main() {
-  cout << "expect 5 " << endl << solution(10,4) << endl;
-  cout << "expect 4 " << endl << solution (24, 18) << endl;
+  cout << "expect 5 " << endl << solution(10, 4) << endl;
+  cout << "expect 4 " << endl << solution(24, 18) << endl;
   return 0;
 }
 
@@ -68,3 +81,9 @@ int main() {
 // N-start/interval - add this if start has not been reached before
 // get next start
 // interval - (n-start)%interval
+
+// new plan
+// count how many intervals
+// we will increment by this many choclates each time
+// when we increment over N, reset the pointer.
+// stop when the pointer is back to the start (or N)
