0dd204ef4ac03d538fd70eb225f02f52f7a5a311
Raskolnikov <jerry.tann@endress.com>
2023-06-02 14:51:08 +0800

    🔥 redo first step, brute force

---
+++ 12eulcidean/chocolates.cpp
@@ -0,0 +1,70 @@
+/*
+
+Two positive integers N and M are given. Integer N represents the number of chocolates arranged in a circle, numbered from 0 to N − 1.
+
+You start to eat the chocolates. After eating a chocolate you leave only a wrapper.
+
+You begin with eating chocolate number 0. Then you omit the next M − 1 chocolates or wrappers on the circle, and eat the following one.
+
+More precisely, if you ate chocolate number X, then you will next eat the chocolate with number (X + M) modulo N (remainder of division).
+
+You stop eating when you encounter an empty wrapper.
+
+For example, given integers N = 10 and M = 4. You will eat the following chocolates: 0, 4, 8, 2, 6.
+
+The goal is to count the number of chocolates that you will eat, following the above rules.
+
+Write a function:
+
+int solution(int N, int M);
+
+that, given two positive integers N and M, returns the number of chocolates that you will eat.
+
+For example, given integers N = 10 and M = 4. the function should return 5, as explained above.
+
+Write an efficient algorithm for the following assumptions:
+
+N and M are integers within the range [1..1,000,000,000].
+
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int solution(int N, int M) {
+  if (N == 0) return 0;
+
+  int cur = 0;
+  bool start = false;
+  int iterations = 0;
+  int count = 0;
+  while (iterations < 200) {
+    iterations++;
+    if ((cur == 0 || cur == N) && start) {
+      return count;
+    }
+    start = true;
+    cur+=M;
+
+    count++;
+    if (cur > N) {
+      cur = cur%N;
+    }
+
+  }
+
+  return count;
+}
+
+int main() {
+  cout << "expect 5 " << endl << solution(10,4) << endl;
+  cout << "expect 4 " << endl << solution (24, 18) << endl;
+  return 0;
+}
+
+// start with 1 (0th candy)
+// count intervals from start to N
+// N-start/interval - add this if start has not been reached before
+// get next start
+// interval - (n-start)%interval
